1d4cf4a5072d241580906aa708a923361f121027
sherlynng <sherlyn.xinler@gmail.com>
2018-10-19 12:15:50 +0800

    Add keyboard for performance

--- js/vimu.js
+++ js/vimu.js
@@ -7,11 +7,13 @@ $( document ).ready(function() {
 function toggleEditing() {
     if (isEditing) {
         $('#editingScreen').hide();
+        $('#myCanvas').show();
         isEditing = false;
         $('#lock').removeClass('active-btn');
         $('#lock > i').removeClass('fa-unlock').addClass('fa-lock');
     } else {
         $('#editingScreen').show();
+        $('#myCanvas').hide();
         isEditing = true;
         $('#lock').addClass('active-btn');
         $('#lock > i').removeClass('fa-lock').addClass('fa-unlock');
