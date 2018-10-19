e5066003f9fe8f81c9411894d6a9e25678095855
sherlynng <sherlyn.xinler@gmail.com>
2018-10-19 15:27:52 +0800

    Define and set layout for components in html

--- js/vimu.js
+++ js/vimu.js
@@ -1,21 +1,38 @@
 var isEditing = false;
 
 $( document ).ready(function() {
-    $('#editingScreen').hide();
+    $('#customization-container').hide();
+    $('#file-directory').hide();
 });
 
 function toggleEditing() {
     if (isEditing) {
-        $('#editingScreen').hide();
-        $('#myCanvas').show();
+        // navigation bar
         isEditing = false;
         $('#lock').removeClass('active-btn');
         $('#lock > i').removeClass('fa-unlock').addClass('fa-lock');
+
+        // layout
+        $('#myCanvas').show();
+        $('#customization-container').hide();
+        $('#file-directory').hide();
+        $('#keyboard-container').removeClass('col-9'); // make keyboard back to full width
+        $('.keyboard-key-edit').removeClass('keyboard-key-edit').addClass('keyboard-key');
+        $('.keyboard-row-edit').removeClass('keyboard-row-edit').addClass('keyboard-row');
+
     } else {
-        $('#editingScreen').show();
-        $('#myCanvas').hide();
+        // navigation bar
         isEditing = true;
         $('#lock').addClass('active-btn');
         $('#lock > i').removeClass('fa-lock').addClass('fa-unlock');
+
+        // layout
+        $('#myCanvas').hide();
+        $('#customization-container').show();
+        $('#file-directory').show();
+
+        $('#keyboard-container').addClass('col-9'); // make keyboard smaller
+        $('.keyboard-key').removeClass('keyboard-key').addClass('keyboard-key-edit');
+        $('.keyboard-row').removeClass('keyboard-row').addClass('keyboard-row-edit');
     }
 }
